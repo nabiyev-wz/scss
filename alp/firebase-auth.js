d3e6106d1d6ff3c3ea90279f8b8645ee2855ae0f
nabiyev-wz <nabiyevkamron282@gmail.com>
2025-02-20 21:25:26 +0500

    il

---
+++ alp/firebase-auth.js
@@ -0,0 +1,4 @@
+document.getElementById("google-signin").addEventListener("click", () => {
+    console.log("OK"); // Konsolda "OK" chiqadi
+    window.location.href = "otp-verification.html"; // Kod kiritish sahifasiga o'tish
+});
